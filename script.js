a0df4f1f59afbf56b6e429dc794d4be7e7b42b05
Víctor Arias <vik@Victors-MacBook-Pro.local>
2019-02-08 23:47:33 -0500

    Add initial files

---
+++ script.js
@@ -0,0 +1,10 @@
+const koopa = document.querySelector('#koopa');
+const timeLine = new TimelineMax();
+
+timeLine.to(koopa, 2,
+    {
+        backgroundPosition: '5830px',
+        ease: SteppedEase.config(22),
+        repeat: 3
+
+})
